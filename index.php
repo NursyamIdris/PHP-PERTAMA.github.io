0de3254454bb19f7e3099990954aaf61f0c1ff93
MaulanaBM <soulm39@gmail.com>
2021-12-19 14:59:29 +0800

    update

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
@@ -17,15 +18,27 @@
 
         $counter = 1;
 
+        echo
+        
+            '<table class="head-hover">'.
+                '<tr class="head-color">'.
+                    '<td class="head hoving-1">'. 'No' .'</td>'.
+                    '<td class="nama hoving-1">'. 'Nama' .'</td>'.
+                    '<td class="nama hoving-1">'. 'Jurusan' .'</td>'.
+                    '<td class="nama hoving-1">'. 'Alamat' .'</td>'.
+                '</tr>'.
+            '</table>';
+
         while($counter <= 10){
+
             echo 
                 '<div>'.
-                    '<table border = 1>'.
+                    '<table class="tabel">'.
                         '<tr>'.
-                            '<td>'. $counter .'</td>'.
-                            '<td>'. $nama .'</td>'.
-                            '<td>'. $jurusan .'</td>'.
-                            '<td>'. $alamat .'</td>'.
+                            '<td class="no hoving-2">'. $counter .'</td>'.
+                            '<td class="nama hoving-2">'. $nama .'</td>'.
+                            '<td class="nama hoving-2">'. $jurusan .'</td>'.
+                            '<td class="nama hoving-2">'. $alamat .'</td>'.
                         '</tr>'.
                     '</table>'.
                 '</div>';
@@ -39,7 +52,6 @@
             
         </div>
 </body>
-
 
 </html>
 
